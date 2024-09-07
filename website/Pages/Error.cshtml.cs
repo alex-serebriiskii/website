64e91d3c1ebb23dbb983cfce25a26f9951cfcb07
Alex <alex.serebriiskii@gmail.com>
2024-09-07 14:31:34 -0400

    Deleted boilerplate and added bulma

--- website/Pages/Error.cshtml.cs
+++ website/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace website.Pages;
+namespace Website.Pages;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
